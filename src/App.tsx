dce8efc74d5cb4e0a4a6824a764c929c5c859849
Beka-Merebashvili <beqamerebashvili26@gmail.com>
2023-10-01 17:22:04 +0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,54 @@
+ 
+ 
+ 
+
+function App() {
+   
+
+  return (
+    <>
+   <div className="overflow-x-auto">
+  <table className="table">
+    {/* head */}
+    <thead>
+      <tr>
+        <th></th>
+        <th>Name</th>
+        <th>Job</th>
+        <th>Favorite Color</th>
+        <th>mail</th>
+      </tr>
+    </thead>
+    <tbody>
+      {/* row 1 */}
+      <tr>
+        <th>1</th>
+        <td>Cy Ganderton</td>
+        <td>Quality Control Specialist</td>
+        <td>Blue</td>
+        <td>rugaca</td>
+      </tr>
+      {/* row 2 */}
+      <tr>
+        <th>2</th>
+        <td>Hart Hagerty</td>
+        <td>Desktop Support Technician</td>
+        <td>Purple</td>
+        <td>rugaca</td>
+      </tr>
+      {/* row 3 */}
+      <tr>
+        <th>3</th>
+        <td>Brice Swyre</td>
+        <td>Tax Accountant</td>
+        <td>Red</td>
+        <td>rugaca</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+    </>
+  )
+}
+
+export default App
