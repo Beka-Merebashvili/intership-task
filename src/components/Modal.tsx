f943c6c8b15a2864d31ec85a09b2154b699c473d
Beka Merebashvili <beqamerebashvili26@gmail.com>
2023-10-03 14:13:20 +0400

    add form in modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -11,7 +11,12 @@ export default function Modal(props : ModalProps) {
       <button onClick={() => props.setModalOpen(false)} className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
     </form>
     <h3 className="font-bold text-lg">Add User</h3>
-    <p className="py-4">aq rame sxva</p>
+   <form className="flex flex-col gap-2">
+    <label>name <input type="text" /></label>
+    <label>mail<input type="text" /></label>
+    <label>city<input type="text" /></label>
+    <button className="btn" type="submit">add</button>
+   </form>
   </div>
 </dialog>
     )
